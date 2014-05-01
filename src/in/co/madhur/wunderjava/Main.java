23389beffde998d595ea23e374752d26461bec3b
Madhur <ahuja.madhur@gmail.com>
2014-05-01 23:18:50 +0530

    added

--- src/in/co/madhur/wunderjava/Main.java
+++ src/in/co/madhur/wunderjava/Main.java
@@ -3,7 +3,9 @@ package in.co.madhur.wunderjava;
 import in.co.madhur.wunderjava.api.AuthException;
 import in.co.madhur.wunderjava.api.NetworkException;
 import in.co.madhur.wunderjava.api.WunderList;
+import in.co.madhur.wunderjava.api.model.Me;
 import in.co.madhur.wunderjava.api.model.WList;
+import in.co.madhur.wunderjava.api.model.WTask;
 
 import java.util.List;
 import java.util.Scanner;
@@ -30,6 +32,14 @@ public class Main
 		{
 			wunderList=WunderList.getInstance(username, password);
 			
+			
+			Me me=wunderList.GetUserInfo();
+			System.out.println("Name: "+ me.getName());
+			System.out.println("Authorization Toekn: "+ me.getToken());
+			
+			System.out.println("Press enter to print out the lists");
+			in.nextLine();
+			
 			List<WList> wlists=wunderList.GetLists();
 			System.out.println("Printing out the lists: ");
 			for(WList list : wlists)
@@ -37,6 +47,23 @@ public class Main
 				System.out.println(list.getTitle());
 				
 			}
+			
+			System.out.println("Press enter to print out the tasks");
+			
+			in.nextLine();
+			
+			
+			List<WTask> wTasks=wunderList.GetTasks();
+			System.out.println("Printing out the Tasks: ");
+			for(WTask task : wTasks)
+			{
+				System.out.println(task.getTitle());
+				
+			}
+			
+			
+			System.out.println("Creating a test task...");
+			//wunderList.
 		}
 		catch (AuthException | NetworkException e)
 		{
